4018d46d40bca045810f9a3b94d0fd58a75db9e8
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2023-07-12 10:15:41 -0300

    added promotional image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <h1>Dust dune</h1>
 
+<img src="call_to_mayhem_ant.png" alt="website promotional material" />
+
 <div>
   <a href="https://www.linkedin.com/company/dustdune">LinkedIn</a>
   <a href="https://www.instagram.com/dustdunegames">Instagram</a>
