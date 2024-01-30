a02390429477f2c194dd81e3b9da30e941939e78
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2024-01-30 19:57:50 -0300

    deleted image from home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,85 +1,78 @@
 <script>
-  import Footer from '../components/footer.svelte'
+	import Footer from '../components/footer.svelte';
 </script>
 
 <header>
-  <div class="inner-header">
-    <h1>DUST DUNE</h1>
-    <div>
-      <button on:click={() => console.log('home')}>Home</button>
-      <button on:click={() => console.log('about')}>About us</button>
-      <button on:click={() => console.log('contact')}>Contact</button>
-    </div>
-  </div>
+	<div class="inner-header">
+		<h1>DUST DUNE</h1>
+		<div>
+			<button on:click={() => console.log('home')}>Home</button>
+			<button on:click={() => console.log('about')}>About us</button>
+			<button on:click={() => console.log('contact')}>Contact</button>
+		</div>
+	</div>
 </header>
 
 <main>
-  <div class="header-space"/>
-  <img src="call_to_mayhem_ant.png" alt="call_to_mayhem_image" />
+	<div class="header-space" />
 </main>
 
 <Footer />
 
 <style>
-  :global(body) {
-    color: #fff;
-    margin: 0;
-    padding: 0;
-  }
+	:global(body) {
+		color: #fff;
+		margin: 0;
+		padding: 0;
+	}
 
-  main {
-    width: 100vw;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  }
+	main {
+		width: 100vw;
+		min-height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
 
-  header {
-    position: fixed;
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    background-color: #06162C;
-  }
+	header {
+		position: fixed;
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		background-color: #06162c;
+	}
 
-  .header-space {
-    height: 100px;
-  }
+	.header-space {
+		height: 100px;
+	}
 
-  .inner-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    max-width: 1200px;
-  }
+	.inner-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		max-width: 1200px;
+	}
 
-  h1 {
-    font-size: 42px;
-    margin-left: 20px;
-  }
+	h1 {
+		font-size: 42px;
+		margin-left: 20px;
+	}
 
-  button {
-    background-color: #06162C;
-    color: #fff;
-    border: none;
-    outline: none;
-    padding: 0px;
-    margin: 20px;
-    cursor: pointer;
-    font-size: 16px;
-  }
+	button {
+		background-color: #06162c;
+		color: #fff;
+		border: none;
+		outline: none;
+		padding: 0px;
+		margin: 20px;
+		cursor: pointer;
+		font-size: 16px;
+	}
 
-  button:hover {
-    color: #64ffda;
-  }
-
-  a {
-    color: #64ffda;
-    text-decoration: none;
-  }
+	button:hover {
+		color: #64ffda;
+	}
 </style>
-
