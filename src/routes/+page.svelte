fe9b5556abf37d65a531021e71842e68ab360285
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2024-06-18 10:08:43 -0300

    mari message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,24 @@
 	import Footer from '../components/footer.svelte';
 </script>
 
-<header>
-	<div class="inner-header">
-		<h1>dust dune</h1>
-		<div>
-			<button on:click={() => console.log('home')}>Home</button>
-			<button on:click={() => console.log('about')}>About us</button>
-			<button on:click={() => console.log('contact')}>Contact</button>
-		</div>
-	</div>
-</header>
+<!-- <header> -->
+<!-- 	<div class="inner-header"> -->
+<!-- 		<h1>dust dune</h1> -->
+<!-- 		<div> -->
+<!-- 			<button on:click={() => console.log('home')}>Home</button> -->
+<!-- 			<button on:click={() => console.log('about')}>About us</button> -->
+<!-- 			<button on:click={() => console.log('contact')}>Contact</button> -->
+<!-- 		</div> -->
+<!-- 	</div> -->
+<!-- </header> -->
 
 <main>
 	<div class="header-space" />
+	<h class="message">Vai dar td certo Mari, minha princesa 💖</h>
+	<img
+		src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjVsMmI2ZHFob3d5ZWJjaTQyZTBmcmtjbmUwM2Fkb25ydDF0cnpjbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9G0AdBbVrkV3O/giphy.webp"
+		alt="Vai dar td certo Mari, minha princesa 💖"
+	/>
 </main>
 
 <Footer />
@@ -28,11 +33,16 @@
 
 	main {
 		width: 100vw;
-		min-height: 100vh;
+		min-height: 80vh;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
+	}
+
+	.message {
+		font-size: 24px;
+		color: #000;
 	}
 
 	header {
