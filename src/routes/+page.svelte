001fe94d630570d3b96db41b59c02892d6075b09
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2024-08-24 14:26:21 -0300

    [RELEASE] 1.0.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
-<script>
-	import Footer from '../components/footer.svelte';
-</script>
-
 <main>
-	<h class="message">In development</h>
+	<div class="fix-vertical">
+		<div class="placeholderdiv">
+			<div class="person">
+				<img width="100%" src="images/logo.svg" alt="logo" />
+			</div>
+			<div class="textcontainer">
+				<h1>Dust Dune</h1>
+				<p>Desenvolva seu website ou aplicativo conosco. Entre em contato e faça um orçamento.</p>
+				<a href="mailto:contact@dustdune.com"> Fazer Orçamento </a>
+			</div>
+		</div>
+	</div>
 </main>
-
-<Footer />
 
 <style>
 	:global(body) {
@@ -20,17 +25,86 @@
 		font-family: 'Roboto', sans-serif;
 	}
 
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
 	main {
-		width: 100%;
-		min-height: 100vh;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
+		min-height: 100vh;
+		width: 100%;
+		background: radial-gradient(circle, #290a72, #0a043c);
+		position: relative;
+		font-family: 'poppins', sans-serif;
+		overflow-x: hidden;
+		overflow-y: scroll !important;
 	}
 
-	.message {
-		font-size: 24px;
+	.person {
+		width: 20vw;
+		margin-right: 2rem;
+
+		@media (max-width: 1000px) {
+			margin-right: 0rem;
+			margin-bottom: 2rem;
+		}
+	}
+
+	a {
 		color: #000;
+		font-weight: bold;
+		font-size: 1.2rem;
+		text-align: center;
+		text-decoration: none;
+		background: #fff;
+		padding: 1rem 1rem;
+		border-radius: 0.5rem;
+		display: inline-block;
+		margin-top: 1rem;
+		box-shadow: 0 0 5rem rgba(1, 1, 1, 0.4);
+	}
+
+	.textcontainer {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.fix-vertical {
+		height: 100%;
+		width: 100%;
+		overflow-x: hidden;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.placeholderdiv {
+		max-width: 60rem;
+		display: flex;
+		align-items: center;
+		background-color: #222222;
+		padding: 2rem 4rem;
+		border-radius: 0.5rem;
+		box-shadow: 0 0 5rem rgba(0, 0, 0, 0.4);
+
+		@media (max-width: 1000px) {
+			flex-direction: column;
+			max-width: 70%;
+		}
+
+		@media (max-width: 800px) {
+			flex-direction: column;
+			max-width: 90%;
+		}
+
+		@media (max-width: 500px) {
+			padding: 2rem;
+			width: 80%;
+		}
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
